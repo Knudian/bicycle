bab87d4ff17813ca1eed2b0519aeface48362b94
robin tusseau <robin.tusseau@gmail.com>
2017-10-02 11:26:36 +0200

    Add api url in meteo/config.js Suppress key in pollution/config.js

--- meteo/config.js
+++ meteo/config.js
@@ -1,1 +1,2 @@
 const openWeatherMapApiKey='ea7baa16bfa72f50eaf63c7771bd1858';
+const openWeatherMapApiUri='http://api.openweathermap.org/data/2.5/';
--- pollution/config.js
+++ pollution/config.js
@@ -1,1 +1,1 @@
-const openWeatherMapApiKey='ea7baa16bfa72f50eaf63c7771bd1858';
+
