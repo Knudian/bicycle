9cf2aa37db509c65259e17dbfa1cf0785b13250a
Knud <kevin.meche.pro@gmail.com>
2017-10-02 14:54:18 +0200

    Starting the app with React

--- app/server.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const server = http.createServer((req,res) => {
-    res.writeHead(200);
-    res.end('test');
-});
-server.listen(6500);
---
+++ app/src/Components/Weather.js
@@ -0,0 +1,11 @@
+import React, { Component, } from 'react';
+import {Well} from "react-bootstrap";
+
+export default class Weather extends Component {
+
+    render(){
+        return(
+            <Well bsSize="large">Look I'm in a large well!</Well>
+        );
+    }
+}
