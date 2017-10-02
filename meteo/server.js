1fcebc32c780f59c3773351fd58840c49470cea4
Knud <kevin.meche.pro@gmail.com>
2017-10-02 16:52:31 +0200

    Updating the meteo services basic informations

--- meteo/server.js
+++ meteo/server.js
@@ -1,8 +1,14 @@
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = process.env.PORT || 3001,
     bodyParser = require('body-parser');
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
