981a004576d017e261f5bd969f3587ad0354cfb7
Knud <kevin.meche.pro@gmail.com>
2017-10-02 16:52:43 +0200

    Revert "evolving app.js"
    This reverts commit 65b23549db2bb7c2e7876fa3a1fe7f689578610f.

--- app.js
+++ app.js
@@ -2,35 +2,15 @@
 
 const http = require('http');
 
-http.createServer((request, response) => {
-    const { headers, method, url } = request;
+const hostname = '127.0.0.1';
+const port = 3000;
 
-    let body = [];
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text-plain');
+    res.end('Hello World\n');
+});
 
-    request.on('error', (err) => {
-        console.err(err);
-    }).on('data', (chunk) => {
-        body.push(chunk);
-    }).on('end', () => {
-        body = Buffer.concat(body).toString();
-        // BEGINNING OF NEW STUFF
-
-        response.on('error', (err) => {
-            console.error(err);
-        });
-
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        // Note: the 2 lines above could be replaced with this next one:
-        // response.writeHead(200, {'Content-Type': 'application/json'})
-
-        const responseBody = { headers, method, url, body };
-
-        response.write(JSON.stringify(responseBody));
-        response.end();
-        // Note: the 2 lines above could be replaced with this next one:
-        // response.end(JSON.stringify(responseBody))
-
-        // END OF NEW STUFF
-    });
-}).listen(3000);
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+})
