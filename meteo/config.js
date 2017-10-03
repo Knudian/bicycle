13fbe7bab26c4c65fd68ebc28d94a409648dfbfb
Knud <kevin.meche.pro@gmail.com>
2017-10-03 12:07:57 +0200

    Putting Meteo Service in another repository

--- meteo/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = Object.freeze({
-    openWeatherMapApiKey: 'ea7baa16bfa72f50eaf63c7771bd1858',
-    openWeatherMapServerUri :'http://api.openweathermap.org/' ,
-    openWeatherMapApiUri: 'data/2.5/'
-});
